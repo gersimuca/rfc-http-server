e85ff9a84394091fa258f08f5adda35712c379b4
UB Gersi Muca <gersimuca@aol.com>
2026-06-24 15:48:00 -0400

    feat(): read from reader and parse

--- main.go
+++ main.go
@@ -3,36 +3,50 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesFromReader(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) > 0 {
+			out <- str
+		}
+	}()
+	return out
+}
 
 func main() {
 	f, err := os.Open("message.txt")
 	if err != nil {
 		log.Fatal("Error", "Error:", err)
 	}
-
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-
-		str += string(data)
-	}
-
-	if len(str) > 0 {
-		fmt.Printf("read: %s\n", str)
+	lines := getLinesFromReader(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n\n", line)
 	}
 }
